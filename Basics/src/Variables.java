0f712aabc712d5c61ea3a89821adfeaa1dc74608
Sergio Marín <sergemb87@gmail.com>
2024-01-28 12:56:24 +0100

    primer commit

---
+++ Basics/src/Variables.java
@@ -0,0 +1,14 @@
+public class Variables {
+    public static void main(String[] args) {
+
+        //Declarando una variable y asignando un valor
+        int speed = 10;
+        System.out.println(speed);
+
+        int salary = 1000;
+        String employeeName = "Sergio";
+        System.out.println(employeeName);
+
+
+    }
+}
